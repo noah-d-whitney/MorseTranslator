49d161aabb55d10366c936bcbaa6783cbcf05d8e
noah-d-whitney <me@noahwhitney.com>
2024-01-25 17:43:23 -0500

    check for invalid input

--- main.go
+++ main.go
@@ -2,93 +2,47 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"unicode"
 )
 
 func main() {
 	fmt.Println("Welcome to morse translator")
 	fmt.Println("Enter english text")
 
-	dot, dash, lSpace, wSpace := "\u2022", "\u2013",
-		"\u0020", "\u0020\u0020"
+	var input string
+	var output *string
 
-	englishMap := map[string]string{
-		"a":  dot + dash,
-		"b":  dash + dot + dot,
-		"c":  dash + dot + dash + dot,
-		"d":  dash + dot + dot,
-		"e":  dot,
-		"f":  dot + dot + dash + dot,
-		"g":  dash + dash + dot,
-		"h":  dot + dot + dot + dot,
-		"i":  dot + dot,
-		"j":  dot + dash + dash + dash,
-		"k":  dash + dot + dash,
-		"l":  dot + dash + dot + dot,
-		"m":  dash + dash,
-		"n":  dash + dot,
-		"o":  dash + dash + dash,
-		"p":  dot + dash + dash + dot,
-		"q":  dash + dash + dot + dash,
-		"r":  dot + dash + dot,
-		"s":  dot + dot + dot,
-		"t":  dash,
-		"u":  dot + dot + dash,
-		"v":  dot + dot + dot + dash,
-		"w":  dot + dash + dash,
-		"x":  dash + dot + dot + dash,
-		"y":  dash + dot + dash + dash,
-		"z":  dash + dash + dot + dot,
-		"0":  dash + dash + dash + dash + dash,
-		"1":  dot + dash + dash + dash + dash,
-		"2":  dot + dot + dash + dash + dash,
-		"3":  dot + dot + dot + dash + dash,
-		"4":  dot + dot + dot + dot + dash,
-		"5":  dot + dot + dot + dot + dot,
-		"6":  dash + dot + dot + dot + dot,
-		"7":  dash + dash + dot + dot + dot,
-		"8":  dash + dash + dash + dot + dot,
-		"9":  dash + dash + dash + dash + dot,
-		".":  dot + dash + dot + dash + dot + dash,
-		",":  dash + dash + dot + dot + dash + dash,
-		"?":  dot + dot + dash + dash + dot + dot,
-		"'":  dot + dash + dash + dash + dash + dot,
-		"!":  dash + dot + dash + dot + dash + dash,
-		"/":  dash + dot + dot + dash + dot,
-		"(":  dash + dot + dash + dash + dot,
-		")":  dash + dot + dash + dash + dot + dash,
-		"&":  dot + dash + dot + dot + dot,
-		":":  dash + dash + dash + dot + dot + dot,
-		";":  dash + dot + dash + dot + dash + dot,
-		"=":  dash + dot + dot + dot + dash,
-		"+":  dot + dash + dot + dash + dot,
-		"-":  dash + dot + dot + dot + dot + dash,
-		"_":  dot + dot + dash + dash + dot + dash,
-		"\"": dot + dash + dot + dot + dash + dot,
-		"$":  dot + dot + dot + dash + dot + dot + dash,
-		"@":  dot + dash + dash + dot + dash + dot,
-		" ":  wSpace,
-	}
-
-	var output string
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	err := scanner.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
-	input := scanner.Text()
-	translate(&input, &output, englishMap)
-	println(output, lSpace)
+	input = scanner.Text()
+
+	err, output = translate(&input, ituMap)
+	if err != nil {
+		log.Fatal(err)
+	}
+	println(*output, lSpace)
 }
 
-func translate(input *string, output *string, charMap map[string]string) *string {
+func translate(input *string, charMap map[string]string) (error, *string) {
+	var err error
+	var output string
 	for _, c := range *input {
-		*output += charMap[string(c)]
-		*output += "\u0020"
+		normalizedC := string(unicode.ToLower(c))
+		val, ok := charMap[normalizedC]
+		if !ok {
+			err = errors.New("Unsupported character: " + normalizedC)
+		}
+		output += val
+		output += "\u0020"
 	}
 
-	return output
+	return err, &output
 }
